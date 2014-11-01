0009e3ce377d288ed6e5773c3af6e1dc4249ca42
Aaronepower <theaaronepower@gmail.com>
2014-11-01 15:31:58 +0000

    testcode

--- routes/users.js
+++ routes/users.js
@@ -1,32 +1,28 @@
 var express = require('express')
   , router  = express.Router()
   , User    = require('../bin/User')
-/* GET users listing. */
 router
 .get('/', function(req, res, next) {
 	User.find(function(err, users) {
 		if (err) res.send(err)
 
-		res.json(users)
+			res.json(users)
 	})
 })
 .post('/' ,function(req, res) {
-		console.log('started')
-		console.log(req.body)
-		var user = new User() 		// create a new instance of the user model
-		user.username = req.body.username  // set the users name (comes from the request)
-		user.password = req.body.password
-		user.avatar   = req.body.avatar
-		console.log(user);
+	console.log('started')
+	console.log(req.body)
+	var user = new User() 		
+	user.username = req.body.username
+	user.password = req.body.password
+	user.avatar   = req.body.avatar
 
-		// save the user and check for errors
-		user.save(function(err) {
-			if (err)
-				res.send(err)
+	user.save(function(err) {
+		if (err)
+			res.send(err)
 
-			res.json(User)
-		})
-		
+		res.json(User)
 	})
+})
 
 module.exports = router
