443cc02c2e03cc09baa661669fa382d2a88652f8
Aaronepower <theaaronepower@gmail.com>
2014-12-05 11:43:20 +0000

    Update to clarify the prototype's UI. UNTESTED.

--- views/js/webrtc.js
+++ views/js/webrtc.js
@@ -1,16 +1,26 @@
 (function () {
   'use strict';
-  rtc.connect('ws://localhost:8001');
+  var roomNum = alert('Enter room number')
+  rtc.connect('ws://localhost:8001', roomNum)
 
   rtc.createStream({'video': true, 'audio':false}, function (stream){
     // get local stream for manipulation
-    rtc.attachStream(stream, 'local');
-  });
+    rtc.attachStream(stream, 'local')
+  })
 
-  var numOfVideos = 0;
+  var numOfVideos = 0
   rtc.on('add remote stream', function (stream){
     // show the remote video
     numOfVideos++
-    rtc.attachStream(stream, 'video'+numOfVideos)
+    var newVideo = document.createElement('video')
+      , width    = 400
+      , height   = 400
+
+    newVideo.autoplay = true
+    newVideo.width = width
+    newVideo.height = height
+    newVideo.id = 'videoNum'+numOfVideos
+    document.getElementById('videoContainer').appendChild(newVideo)
+    rtc.attachStream(stream, newVideo.id)
   })
 })()
